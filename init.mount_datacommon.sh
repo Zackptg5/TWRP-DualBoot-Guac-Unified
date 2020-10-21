ececf221d610b821fc8f538e9c33c710ac50e2a7
lvismara <lorenzo.vismara@luxottica.com>
2020-10-21 12:19:34 +0200

    commondata A11 fixed

--- init.mount_datacommon.sh
+++ init.mount_datacommon.sh
@@ -16,7 +16,11 @@ until [ -d /storage/emulated/0/Android ]; do
 done
 # Make one folder that has everything in it - for files not in a folder
 mkdir /storage/emulated/0/CommonData 2>/dev/null
-mount -t sdcardfs -o rw,nosuid,nodev,noexec,noatime,fsuid=1023,fsgid=1023,gid=9997,mask=7,derive_gid,default_normal /datacommon /storage/emulated/0/CommonData
+if [ -f "/system/etc/init/hw/init.rc" ]; then
+  mount -t sdcardfs -o rw,nosuid,nodev,noexec,noatime,fsuid=1023,fsgid=1023,gid=9997,mask=7,derive_gid,default_normal /datacommon /mnt/pass_through/0/emulated/0/CommonData
+else
+  mount -t sdcardfs -o rw,nosuid,nodev,noexec,noatime,fsuid=1023,fsgid=1023,gid=9997,mask=7,derive_gid,default_normal /datacommon /storage/emulated/0/CommonData
+fi
 # Mount folders over top - sdcardfs only supports directory mounting
 [ -f /datacommon/mounts.txt ] || exit 0
 if [ "$(head -n1 /datacommon/mounts.txt | tr '[:upper:]' '[:lower:]')" == "all" ]; then
